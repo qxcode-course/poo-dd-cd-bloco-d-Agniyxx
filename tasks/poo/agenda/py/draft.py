3acc556136bb1bb6cf45c86a384f52aabb06bf01
Agne <agne.steph.moss@gmail.com>
2025-11-23 17:59:22 -0300

    fazendo atvd da agenda

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -1,17 +1,52 @@
 class Fone:
-    def __init__(self, nome: str, fone: int):
-        self.__nome = nome
-        self.__numero = fone
+    def __init__(self, id: str, number: int):
+        self.__id = id
+        self.__number = number
+        
+        def getId(self):
 
-        def __str__(self):
-            return f"{self.__nome}:{self.__numero}"
+        def getNumber(self):
+         
+
+
+         def __str__(self):
+            return f"{self.__id}:{self.__number}"
 
 class Contact:
-    def __init__(self):
-        self.favoritos: Fone | None = []
+    def __init__(self, name: str):
+        self.name = name
+        self.fones = []
+        self.isFavorits = False
 
+        def addFone(self, id: str, number: str):
+
+        def rmFone(self, index: int):
+
+        def toogleFavorited(self):
+
+        def isFavoroted(self):
+
+
+        def __str__(self):
+            fones_str = ", ".join(str(f) for f in self.fones)
+            return f"{self.name} : {self.fones_str}"
 
 class Agenda(self):
+    self.contacts: []
+
+        def addContact(self):
+
+        def getContact(self):
+        
+        def rmContact(self):
+        
+        def search(self):
+        
+        def getFavorited(self):
+        
+        def getContacts(self):
+        
+        def __str__(self):
     
 
 
